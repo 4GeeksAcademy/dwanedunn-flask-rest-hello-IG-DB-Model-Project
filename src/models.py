bfddb72465abb8d4e1c6492945a7c85aa0d78fe6
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-21 14:28:22 +0000

    add username to User Model & serialize it

--- src/models.py
+++ src/models.py
@@ -4,16 +4,22 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    # Add a username field
+    username: Mapped[str] = mapped_column(
+        String(80), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # return the username
+            "username": self.username,
             # do not serialize the password, its a security breach
         }
